941a04595eeb3bdd362444c0c412dcecf36dd155
gobeli <etienne.gobeli@outlook.com>
2024-03-28 11:51:36 +0100

    add redirect in app root path

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/gobeli/pocketbase-htmx/middleware"
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 )
@@ -9,6 +10,9 @@ import (
 func InitAppRoutes(e *core.ServeEvent, pb *pocketbase.PocketBase) {
 	appGroup := e.Router.Group("/app", middleware.LoadAuthContextFromCookie(pb), middleware.AuthGuard)
 
+	appGroup.GET("", func(c echo.Context) error {
+		return c.Redirect(303, "profile")
+	})
 	appGroup.GET("/profile", ProfileGet)
 	appGroup.GET("/todos", TodosGet(e))
 	appGroup.GET("/todos/add", TodoAddGet)
